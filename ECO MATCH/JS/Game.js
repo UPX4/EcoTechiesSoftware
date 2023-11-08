3c342abe574ab37bcb178bb8254d1de1066a8622
Leonardo Lima <104847717+sirbickle@users.noreply.github.com>
2023-11-08 17:58:33 -0300

    Finalizado a logica para mostrar as cartas e finalizar o jogo
    Finalizado a logica para mostrar as cartas e finalizar o jogo

--- ECO MATCH/JS/Game.js
+++ ECO MATCH/JS/Game.js
@@ -13,6 +13,71 @@ const createElement = (tag, className) => {
     return element;
   }  
 
+  let firstCard = '';
+  let secondCard = '';
+
+  const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if (disabledCards.length === 8) {
+        alert('Parabéns');
+
+      }
+
+  }
+
+  const checkCards = () => {
+
+    const firstTema = firstCard.getAttribute('data-tema');
+    const secondTema = secondCard.getAttribute('data-tema');
+
+    if (firstTema === secondTema) {
+
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
+
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
+
+    } else {     
+        setTimeout(() => {
+
+        firstCard.classList.remove('reveal-card');
+        secondCard.classList.remove('reveal-card');
+
+        firstCard = '';
+        secondCard = '';
+
+        }, 500);
+    
+
+    }
+}
+
+
+const revealCard = ({ target }) => {
+
+    if (target.parentNode.className.includes('reveal-card')) {
+        return;
+      }
+
+      if (firstCard === '') {
+    
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+    
+      } else if (secondCard === '') {
+
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+
+    }
+}
+
 const createCard = (tema) => {
 
     const card = createElement('div', 'card');
@@ -23,6 +88,9 @@ const createCard = (tema) => {
 
     card.appendChild(front);
     card.appendChild(back);
+
+    card.addEventListener('click', revealCard);
+    card.setAttribute('data-tema', tema)
   
     return card;
 }
